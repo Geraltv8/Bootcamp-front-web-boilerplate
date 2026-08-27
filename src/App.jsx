68ecf859e6d7f6b8a3abbd8d9041b7e19e409656
Geraltv8 <geraquesearock@gmail.com>
2026-08-27 20:53:28 -0300

    Agregado React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import DashboardRecepcion from "./pages/DashboardRecepcion"
 import FormularioPaciente from "./components/pacientes/FormularioPaciente"
+import LayoutPrincipal from './components/layout/LayoutPrincipal';
 
 function App() {
 
   return (
     <>
       <Toaster position="top-right" richColors/>
-      <DashboardRecepcion></DashboardRecepcion>
-      <FormularioPaciente></FormularioPaciente>
+
+      <Routes>
+        <Route path="/" element={<LayoutPrincipal />}>
+          <Route index element={<DashboardRecepcion />} />
+          <Route path="nuevo-paciente" element={<FormularioPaciente />} />
+        </Route> 
+      </Routes>
     </>
   )
 }
 
-export default App
+export default App;
